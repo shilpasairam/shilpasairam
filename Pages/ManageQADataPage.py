4d77629148e104f905e23e61b793563395a4f6f7
SACHIN RANGASWAMY <sachin.rangaswamy@cytel.com>
2022-08-16 14:14:35 +0530

    Merge branch 'master' of https://bitbucket.org/cytelsoftware/pse.autotest into LIVEHTA-846

--- Pages/ManageQADataPage.py
+++ Pages/ManageQADataPage.py
@@ -1,11 +1,15 @@
 import os
+from pathlib import Path
 import time
+import openpyxl
 import pandas as pd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 from Pages.Base import Base
 from Pages.ManagePopulationsPage import ManagePopulationsPage
+from Pages.OpenLiveSLRPage import LiveSLRPage
+from Pages.SLRReportPage import SLRReport
 from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
 from utilities.logScreenshot import cLogScreenshot
@@ -25,6 +29,10 @@ class ManageQADataPage(Base):
         self.logger = LogGen.loggen()
         # Instantiate the logScreenshot class
         self.LogScreenshot = cLogScreenshot(self.driver, self.extra)
+        # Creating object of liveslrpage class
+        self.liveslrpage = LiveSLRPage(self.driver, extra)
+        # Creating object of slrreport class
+        self.slrreport = SLRReport(self.driver, extra)
         # Creating object of ManagePopulationsPage class
         self.mngpoppage = ManagePopulationsPage(self.driver, extra)
         # Instantiate webdriver wait class
@@ -38,7 +46,7 @@ class ManageQADataPage(Base):
         file = pd.read_excel(filepath)
         study_type = list(file['Study_Types'].dropna())
         qa_file = list(os.getcwd()+file['QA_Excel_Files'].dropna())
-        result = [(study_type[i], qa_file[i]) for i in range(0, len(study_type))]
+        result = [[study_type[i], qa_file[i]] for i in range(0, len(study_type))]
         return result
     
     def get_qa_file_details_override(self, filepath):
@@ -202,6 +210,118 @@ class ManageQADataPage(Base):
 
                 stdy_ele = self.select_element("select_stdy_type_dropdown")
                 select = Select(stdy_ele)
+                select.select_by_visible_text(i[0])
+                time.sleep(1)
+
+                self.click(del_locator)
+                time.sleep(2)
+                self.click(del_locator_popup)
+                time.sleep(2)
+
+                actual_delete_status_text = self.get_text("get_status_text", UnivWaitFor=10)
+
+                if actual_delete_status_text == expected_delete_status_text:
+                    self.LogScreenshot.fLogScreenshot(message=f'QA File Deletion is success. Text is {actual_delete_status_text}',
+                                            pass_=True, log=True, screenshot=True)
+                else:
+                    raise Exception("Error in QA file Deletion")
+        except:
+            raise Exception("Unable to delete the existing QA file")
+
+    def compare_qa_file_with_report(self, study_data, pop_value, slrfilepath):
+        expected_upload_status_text = 'QA File successfully uploaded'
+
+        try:
+            for i in study_data:
+                self.refreshpage()
+                self.go_to_manageqadata("manage_qa_data_button")
+                time.sleep(3)
+                pop_ele = self.select_element("select_pop_dropdown")
+                select = Select(pop_ele)
+                select.select_by_visible_text(pop_value)
+                time.sleep(1)
+                pop_value = select.first_selected_option.text
+
+                stdy_ele = self.select_element("select_stdy_type_dropdown")
+                select = Select(stdy_ele)
+                select.select_by_visible_text(i[0])
+                time.sleep(1)
+
+                self.input_text("qa_checklist_name", f"QAName_{pop_value}_{i[0]}")
+                self.input_text("qa_checklist_citation", f"QACitation_{pop_value}_{i[0]}")
+                self.input_text("qa_checklist_reference", f"QAReference_{pop_value}_{i[0]}")
+                self.input_text("qa_excel_file_upload", i[1])
+                time.sleep(2)
+
+                self.click("upload_save_button")
+                time.sleep(2)
+                actual_upload_status_text = self.get_text("get_status_text", UnivWaitFor=10)
+
+                if actual_upload_status_text == expected_upload_status_text:
+                    self.LogScreenshot.fLogScreenshot(message=f'QA File upload is success. Text is {actual_upload_status_text}',
+                                            pass_=True, log=True, screenshot=True)
+                else:
+                    self.LogScreenshot.fLogScreenshot(message=f'Error in file uploading',
+                                            pass_=False, log=True, screenshot=True)
+
+                # Go to live slr page
+                self.liveslrpage.go_to_liveslr("SLR_Homepage")
+                if i[0] == 'Quality of life':
+                    i[0] = 'Quality of Life'
+                self.slrreport.select_data(f"{pop_value}", f"{pop_value}_radio_button")
+                self.slrreport.select_data(i[0], f"{i[0]}_radio_button")
+                self.slrreport.generate_download_report("excel_report")
+                time.sleep(5)
+                excel_filename1 = self.slrreport.getFilenameAndValidate(180)
+                self.slrreport.validate_filename(excel_filename1, slrfilepath)
+
+                excel_data = openpyxl.load_workbook(f'ActualOutputs//{excel_filename1}')
+                excel_sheet = excel_data['Quality Assessment']
+                if excel_sheet['E1'].value == 'Back To Toc':
+                    self.LogScreenshot.fLogScreenshot(message=f"'Back To Toc' option is present",
+                                            pass_=True, log=True, screenshot=False)
+                else:
+                    self.LogScreenshot.fLogScreenshot(message=f"'Back To Toc' option is not present",
+                                            pass_=False, log=True, screenshot=False)
+                
+                toc_sheet = pd.read_excel(f'ActualOutputs//{excel_filename1}', sheet_name="TOC", skiprows=3)
+                col_data = list(toc_sheet.iloc[:, 1])
+                if 'Quality Assessment' in col_data:
+                    self.LogScreenshot.fLogScreenshot(message=f'Quality Assessment is present in TOC sheet.',
+                                            pass_=True, log=True, screenshot=False)
+                else:
+                    self.LogScreenshot.fLogScreenshot(message=f'Quality Assessment is not present in TOC sheet. Available Data from TOC sheet: {col_data}',
+                                            pass_=False, log=True, screenshot=False)
+                
+                qafile = pd.read_excel(i[1])
+                excelfile = pd.read_excel(f'ActualOutputs//{excel_filename1}', sheet_name="Quality Assessment", skiprows=1)
+
+                if qafile.equals(excelfile):
+                    self.LogScreenshot.fLogScreenshot(message=f"File contents between QA File '{Path(f'{i[1]}').stem}' and Complete Excel Report '{Path(f'ActualOutputs//{excel_filename1}').stem}' are matching",
+                                            pass_=True, log=True, screenshot=False)
+                else:
+                    raise Exception("File contents between QA File '{Path(f'{i[1]}').stem}' and Complete Excel Report '{Path(f'ActualOutputs//{excel_filename1}').stem}' are not matching")
+
+        except:
+            raise Exception("Unable to upload the Manage QA Data")
+
+    def del_data_after_qafile_comparison(self, study_data, del_locator, del_locator_popup, pop_value):
+        expected_delete_status_text = 'QA excel file successfully deleted'
+        self.go_to_manageqadata("manage_qa_data_button")
+
+        try:
+            for i in study_data:
+                self.refreshpage()
+                time.sleep(3)
+                pop_ele = self.select_element("select_pop_dropdown")
+                select = Select(pop_ele)
+                select.select_by_visible_text(pop_value)
+                time.sleep(1)
+
+                stdy_ele = self.select_element("select_stdy_type_dropdown")
+                select = Select(stdy_ele)
+                if i[0] == 'Quality of Life':
+                    i[0] = 'Quality of life'
                 select.select_by_visible_text(i[0])
                 time.sleep(1)
 
