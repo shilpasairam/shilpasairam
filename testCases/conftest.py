efbf06fa5e6ceb071965130acb8579c0f0bac2d5
SACHIN RANGASWAMY <sachin.rangaswamy@cytel.com>
2023-05-11 16:52:53 +0530

    HTML to PDF report conversion

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -9,6 +9,8 @@ import platform
 import socket
 import psutil
 import datetime
+import glob
+from pyhtml2pdf import converter
 from pathlib import Path
 from utilities.readProperties import ReadConfig
 from selenium.webdriver.chrome.service import Service
@@ -170,6 +172,12 @@ def pytest_html_results_table_row(report, cells):
 
 @pytest.hookimpl(trylast=True)
 def pytest_sessionfinish(session, exitstatus):
+
+    # Converting the HTML report file to PDF format
+    filename = Path(glob.glob('Reports//*.html')[0]).stem
+    path = os.path.abspath(f'Reports//{filename}.html')
+    converter.convert(f'file:///{path}?collapsed=Skipped', f"{filename}.pdf")
+
     dir_list = ['ActualOutputs', 'Logs', 'Reports']
     zip_name = f"{session.config.getoption('-m')}_results.zip"
 
