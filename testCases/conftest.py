56740e378518624b42db8570e5950c5c595ed5c9
Prasad Dodla <lakshmivvnsdprasadarao.dodla@cytel.com>
2023-05-15 14:00:34 +0000

    Removing the .zip and .pdf files before the test runs

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -103,7 +103,15 @@ def pytest_sessionstart(session):
     if os.path.exists(f'ActualOutputs'):
         for root, dirs, files in os.walk(f'ActualOutputs'):
             for file in files:
-                os.remove(os.path.join(root, file))    
+                os.remove(os.path.join(root, file))
+                
+    # Removing the .zip and .pdf files before the test runs
+    for dirpath, dirnames, filenames in os.walk(os.curdir):
+        for file in filenames:
+            if fnmatch(file, '*.zip'):
+                os.remove(os.path.join(dirpath, file))
+            if fnmatch(file, '*.pdf'):
+                os.remove(os.path.join(dirpath, file))
 
 
 # @pytest.hookimpl(tryfirst=True)
